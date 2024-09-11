9ce74cb7518bbc00392635fdb425d5eecfe3c802
Stalfos1 <xteb4nrock@gmail.com>
2024-09-11 18:53:38 -0500

    Added an interactive menu for the input

--- src/main.py
+++ src/main.py
@@ -1,12 +1,5 @@
 import datetime
 
-plate='pbx1023'
-# day/month/year/
-date='10/9/2024'
-# 24:00 hours format
-time= datetime.time(8, 30)
-print(time)
-print(plate[-1])
 
 def day_date(date):
   # String to datetime
@@ -55,8 +48,55 @@ def pico_placa_detector (plate,date,time):
         return is_vehicle_allowed(day_week,plate)
 
 
+""""
+plate='xyz5678'
+# day/month/year/
+date='10/9/2024'
+# 24:00 hours format
+time= datetime.time(8, 30)
+print(time)
+print(plate[-1])
+
 
 
 print( day_date(date) )
 
 print(pico_placa_detector(plate,date,time))
+
+
+"""
+
+
+def enter_data():
+
+    plate = input("Enter a vehicle plate (example: ABC1234): ")
+    date_input = input("Enter a date (format DD/MM/YYYY): ")
+    
+    try:
+        date = datetime.datetime.strptime(date_input, "%d/%m/%Y").strftime('%d/%m/%Y')
+    except ValueError:
+        print("Error: Invalid date format. Try again.")
+        return
+    
+    time_input = input("Enter the time (format HH:MM, 24 hours): ")
+    
+    try:
+        time = datetime.datetime.strptime(time_input, "%H:%M").time()
+    except ValueError:
+        print("Error: Invalid time format. Try again.")
+        return
+    
+    if pico_placa_detector(plate, date, time):
+        print("The car is allowed to circulate")
+    else:
+        print("The car ISN'T allowed to circulate")
+
+
+if __name__ == "__main__":
+    while True:
+        enter_data()
+        
+        another = input("Do you want to check another vehicle plate? (y/n): ").lower()
+        if another != 'y':
+            print("Good bye!")
+            break
