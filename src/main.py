ba957129132197df3827757c8c537ccc8a64c7ef
Stalfos1 <xteb4nrock@gmail.com>
2024-09-11 00:09:08 -0500

    implement function day_date, return the day of the week of any date

--- src/main.py
+++ src/main.py
@@ -1,1 +1,27 @@
-placa='pbx1024'
+import datetime
+
+plate='pbx1024'
+# day/month/year/
+date='1/8/2023'
+# 24:00 hours format
+time= datetime.time(11, 30)
+print(time)
+
+
+def pico_placa_detector (placa,date,time):
+    
+    print()
+    
+def day_date(date):
+  # String to datetime
+  date_datetime = datetime.datetime.strptime(date, '%d/%m/%Y')
+
+  # Obtain the day of the week number (0=Monday, 6=Sunday)
+  number_day_week = date_datetime.weekday()
+
+  days_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+  day = days_week[number_day_week]
+
+  return day
+
+print( day_date(date) )
